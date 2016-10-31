89f5943a7a408adc852d33cacbb213acf8d91805
RUAN0007 <ruanpingcheng@gmail.com>
2016-10-31 13:07:29 +0800

    Modify prediction_engine
    * Add a GP Process predictor
    * Remove the first identifical segment for knn

--- knn-prediction/prediction.py
+++ knn-prediction/prediction.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
 
 class KnnPrediction:
     def SetSource(self, source):
@@ -10,8 +11,53 @@ class KnnPrediction:
     def GetTopKnn(self, start, end, K):
         scores = self.__CalDtwDist(start, end)
         top_k = self.__SelectTopK(scores, K, end-start)
-        print(top_k)
+#        print(top_k)
         return top_k
+
+    def GP_Predict(self, start,end,top_k):
+        print ("Start GP_Predict: ")
+        k = len(top_k)
+        l = end-start
+        X = [] # Input samples(shape : n_samples * n_features)
+        Y = [] # Expected label (shape: n_samples * look_ahead)
+        for i in range(0,k):
+            offset = top_k[i][1] + l
+            x = []
+
+            for j in range(top_k[i][1], offset):
+                x.append(self._rate[j])
+
+            X.append(x)
+
+            look_ahead = 3
+            y = []
+            for j in range(look_ahead):
+                y.append(self._rate[offset + j])
+            Y.append(y)
+
+        np_X = np.array(X)
+        np_y = np.array(Y)
+        print("np_X = ")
+        print(np_X)
+        print("np_y = ")
+        print(np_y)
+        print()
+
+        gp = GaussianProcessRegressor(n_restarts_optimizer=3)
+
+# Fit to data using Maximum Likelihood Estimation of the parameters
+        gp.fit(np_X, np_y)
+
+        input_segment = [self._rate[start:end]]
+        np_segment = np.array(input_segment)
+        print("np_segment: ")
+        print (np_segment)
+        y_preds,stds = gp.predict(np_segment,return_std=True  )
+        print ("y_pred: ")
+        print(y_preds)
+        print("std: ")
+        print(stds)
+        return y_preds, stds
 
     def Predict(self, start, end, top_k):
         k = len(top_k)
@@ -66,6 +112,7 @@ class KnnPrediction:
                     break
             if ok:
                 ret.append(score)
-                if len(ret) >= K:
+                if len(ret) >= K + 1:
                     break
-        return ret
+        #Remove the first element, which is the same as the input segment
+        return ret[1:]
