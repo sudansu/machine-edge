992dbb55d215d9830c4fd199de5e7f658ae50c4e
WANG Sheng <wangsheng1001@gmail.com>
2016-12-03 17:11:28 +0800

    Add knn prediction core

---
+++ src/core/knn.py
@@ -0,0 +1,154 @@
+import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
+
+from common import utils
+
+class KnnGaussianPrediction:
+    """
+    Give a source and a choosen sub-interval, to find kNN of that change
+    pattern, and predict future plots using Gaussian processing
+
+    Attributes
+    ----------
+      _src: list(float)
+        internal representation of current source (change rate/norm)
+    """
+
+    def __init__(self):
+        pass
+
+    def fit(self, src):
+        """
+        Set source to predict
+
+        Parameters
+        ----------
+          src: list(double)
+            source to predict
+        """
+        self._src = utils.get_source_change_rate(src)
+
+    def get_knn(self, start, end, k):
+        """
+        Given an interval, find k neighbors
+
+        Parameters
+        ----------
+          start: int
+            start position
+          end: int
+            end position
+          k: int
+            number of required neighbors
+
+        Returns
+        -------
+          list([float, int])
+            k tuples in format [distance, position]
+        """
+        scores = self.__cal_distance(start, end)
+        top_k = self.__select_top_k(scores, k, end-start)
+        return top_k
+
+    def predict(self, start, end, top_k, look_ahead):
+        """
+        Predict using Gaussian processing based on top k neighbors
+
+        Parameters
+        ----------
+          start: int
+            start position
+          end: int
+            end position
+          top_k: list([float, int])
+            top k neighbors
+          look_ahead: int
+            number of points to predict
+
+        Returns
+        -------
+          list(float), list(float)
+            predicted values, standart errors
+        """
+        print ("Start Predict...")
+        k = len(top_k)
+        l = end-start
+        X = [] # Input samples(shape : n_samples * n_features)
+        Y = [] # Expected label (shape: n_samples * look_ahead)
+        for i in range(k):
+            offset = top_k[i][1] + l
+            x = []
+            for j in range(top_k[i][1], offset):
+                x.append(self._rate[j])
+            X.append(x)
+            y = []
+            for j in range(look_ahead):
+                y.append(self._rate[offset + j])
+            Y.append(y)
+        np_X = np.array(X)
+        np_Y = np.array(Y)
+        print("np_X = " + str(np_X))
+        print("np_Y = " + str(np_Y))
+        gp = GaussianProcessRegressor(n_restarts_optimizer=3)
+        # Fit to data using Maximum Likelihood Estimation of the parameters
+        gp.fit(np_X, np_Y)
+        input_segment = [self._rate[start:end]]
+        np_segment = np.array(input_segment)
+        print("np_segment: " + str(np_segment))
+        y_preds,stds = gp.predict(np_segment,return_std=True  )
+        print ("y_pred: " + str(y_preds))
+        print("std: " + str(stds))
+        return y_preds[0], stds[0]
+
+    def __cal_distance(self, start, end):
+        """
+        Calculate all possible distances against an interval
+
+        Parameters
+        ----------
+          start: int
+            start position
+          end: int
+            end position
+
+        Returns
+        -------
+          list([float, int])
+            list of tuples in format [distance, position]
+        """
+        l = end - start
+        ret = []
+        matrix = np.empty([l+1, l+1])
+        for i in range(0, len(self._src) - l + 1):
+            score = self.__Dtw(matrix, self._src[start:end], self._src[i:i+l])
+            ret.append([score, i])
+        return ret
+
+    def __select_top_k(self, scores, k, min_dist):
+        """
+        Select k smallest distance
+
+        Parameters
+        ----------
+          scores: list([float, int])
+            sequence of tuples in format [distance, position]
+          k: int
+            number of selected points
+          min_dist: int
+            minimum position difference between any selected points
+        """
+        scores = sorted(scores)
+        ret = []
+        for score in scores:
+            v = score[1]
+            ok = True
+            for old in ret:
+                if (abs(v-old[1]) < min_dist):
+                    ok = False
+                    break
+            if ok:
+                ret.append(score)
+                if len(ret) > k:
+                    break
+        # Remove the first element, which is the same as the input segment
+        return ret[1:]
